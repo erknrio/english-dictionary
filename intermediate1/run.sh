c15d0b3ad9d72cca67fd6cb499239b6ca1b3ba24
root <root@debian>
2018-03-29 20:44:41 +0200

    Updated requirements

--- intermediate1/run.sh
+++ intermediate1/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Activate enviorement"
 source virtualenv/bin/activate
+echo "Database changes"
+./manage.py makemigrations dictionary
+./manage.py migrate
 echo "Start"
-./manage.py runserver 127.0.0.1:8000
+./manage.py runserver 0.0.0.0:8000
