f822a3a5688c6b3c79f9325c0d75c4ccea730e14
Adrián Garrido <adrian.galotecnia@gmail.com>
2018-05-09 11:09:27 +0100

    Bugfix

--- intermediate1/clear-run.sh
+++ intermediate1/clear-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Delete old database and migrations. . ."
 rm db.sqlite3
-rm -rf interface/migrations
-rm -rf interface/__pycache__
+rm -rf dictionary/migrations
+rm -rf dictionary/__pycache__
 rm -rf _autofixture
 echo -e "Done\n"
 echo "Create migrations and database. . ."
