ca3de83a021f6131197b19fd48dad8d3dbf6d61e
Adrián Garrido <adrian.galotecnia@gmail.com>
2018-12-19 18:03:42 +0000

    Simplified for API REST (django rest framework)

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,55 +1,29 @@
-# Views
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-# Models
-from .models import Word
-# Forms
-from .forms import WordForm
-# Extra
-from django.urls import reverse, reverse_lazy
+from rest_framework import viewsets, permissions, mixins
+from .serializers import *
+from .models import *
 
 
-# Word
-class WordList(ListView):
-    model = Word
-    template_name = 'dictionary/words_list.html'
+class LevelViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
 
 
-class WordDetail(DetailView):
-    model = Word
-    template_name = 'dictionary/word_detail.html'
+class CategoryViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    # If we add: mixins.DestroyModelMixin,
+    # we also could delete categories
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
-class WordCreate(CreateView):
-    model = Word
-    form_class = WordForm
-    template_name = "dictionary/word_form.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['header_text'] = "New Word"
-        return context
-
-    def get_success_url(self):
-        return reverse('words_update', kwargs={'pk': self.object.pk})
-
-
-class WordUpdate(UpdateView):
-    template_name = "dictionary/word_form.html"
-    model = Word
-    form_class = WordForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['header_text'] = "Update Word"
-        return context
-
-    def get_success_url(self):
-        return reverse('words_update', kwargs={'pk': self.object.pk})
-
-
-class WordDelete(DeleteView):
-    model = Word
-    template_name = "dictionary/word_confirm_delete.html"
-    success_url = reverse_lazy('words_list')
+class WordViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
