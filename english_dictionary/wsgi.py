10d6bf74a7b02f4e1778e55b63cb009ee38c9a4f
Adrián Garrido <adrian.galotecnia@gmail.com>
2018-12-19 17:28:10 +0000

    Renamed django project

--- english_dictionary/wsgi.py
+++ english_dictionary/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for intermediate1 project.
+WSGI config for english_dictionary project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intermediate1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "english_dictionary.settings")
 
 application = get_wsgi_application()
